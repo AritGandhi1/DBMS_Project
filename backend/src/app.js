19cf2e4cd28b0ec8690c03e18a16a7c32a0b7bb8
Shashank M N <mnshashank432@gmail.com>
2026-04-13 17:13:05 +0530

    Serve frontend from backend and use same-origin API base URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
 
 const routes = require("./routes");
 const notFound = require("./middlewares/notFound");
@@ -12,6 +14,18 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use("/api", routes);
+
+const frontendDir = path.resolve(__dirname, "../../frontend");
+const frontendIndexPath = path.join(frontendDir, "index.html");
+
+if (fs.existsSync(frontendIndexPath)) {
+	app.use(express.static(frontendDir));
+
+	// Serve SPA/static frontend for all non-API paths.
+	app.get(/^\/(?!api).*/, (req, res) => {
+		res.sendFile(frontendIndexPath);
+	});
+}
 
 app.use(notFound);
 app.use(errorHandler);
